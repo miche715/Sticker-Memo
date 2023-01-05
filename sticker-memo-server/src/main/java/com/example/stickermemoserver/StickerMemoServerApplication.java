7600065ec1d827ab1c0f4f58ecb90bf3cfc0db71
Cheel <44915367+miche715@users.noreply.github.com>
2023-01-05 17:00:10 +0900

    Initial commit

---
+++ sticker-memo-server/src/main/java/com/example/stickermemoserver/StickerMemoServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.stickermemoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StickerMemoServerApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(StickerMemoServerApplication.class, args);
+    }
+}
---
+++ sticker-memo-server/src/test/java/com/example/stickermemoserver/StickerMemoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stickermemoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StickerMemoServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
